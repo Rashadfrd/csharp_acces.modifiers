bf98fbe21d615ed9ab11668ee14bc2390c243ff7
Rashadfrd <rashadnf@code.edu.az>
2022-06-02 11:07:45 +0400

    task added

---
+++ task/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name:");
+            User user1 = new User(Console.ReadLine());
+            Console.WriteLine("Enter password:");
+            user1.Password = Console.ReadLine();
+
+            Console.WriteLine($"Name: {user1.UserName}  Password: {user1.Password}");
+            
+
+           
+        }
+    }
+}
---
+++ task/User.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task
+{
+    class User
+    {
+
+        public string UserName;
+        private string _password;
+        public User(string name)
+        {
+            if (name.Length > 5 && name.Length < 26)
+            {
+                this.UserName = name;
+            }
+        }
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (value.Length > 7 && value.Length < 26 && HasDigit(value) && HasUpper(value) && HasLower(value))
+                {
+                    _password = value;
+                }
+            }
+        }
+
+        public bool HasDigit(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(str) && char.IsDigit(str[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool HasUpper(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(str) && char.IsUpper(str[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool HasLower(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(str) && char.IsLower(str[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+    }
+}
